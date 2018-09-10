105a47ba3130a615cc49e91dcb31e579216d0058
Saeed Rahman <saeedrahman@cdnis.edu.hk>
2018-09-10 14:14:19 +0800

    initial commit

--- RandomNumberGenerator/ViewController.swift
+++ RandomNumberGenerator/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var randomNumberLabel: UILabel!
+    
+    
+    // This function does two things: (1) generates a number between 0 and 9 and (2) assigns
+    // the number to the text property of the label
+    func generateRandomNumber() {
+        let randomNumber = arc4random_uniform(10)
+        randomNumberLabel.text = String(randomNumber)
+    }
+    
+    // This action calls the generateRandomNumber function
+    @IBAction func randomNumberButtonAction(_ sender: Any) {
+        generateRandomNumber()
+    }
+    
+    // This function is called when the view has been loaded. It's a great place to call other
+    // functions which needed to be loaded on starting your app. The generateRandomNumber
+    // function is being called on startup.
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        generateRandomNumber()
     }
 
     override func didReceiveMemoryWarning() {
