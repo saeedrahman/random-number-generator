5e239b7a305da2f248e446de02ff603e6c77f10f
Saeed Rahman <saeedrahman@cdnis.edu.hk>
2018-09-10 11:58:35 +0800

    Initial Commit

---
+++ RandomNumberGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomNumberGenerator
+//
+//  Created by Saeed Rahman on 10/9/2018.
+//  Copyright © 2018 Saeed Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
